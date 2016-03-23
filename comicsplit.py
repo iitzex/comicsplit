a6f2cdad21382d62d5c90c8b8847af767d0fe212
iitzex <iitzex@gmail.com>
2016-03-23 11:20:44 +0800

    update for directory structure and zip

--- comicsplit.py
+++ comicsplit.py
@@ -1,13 +1,13 @@
 from PIL import Image
 from glob import glob
 import os
+import zipfile
 
+new_index = 1
+prefix = 'kindle_'
 def split(filename):
-    mainname, subname = os.path.splitext(filename)
-    # print mainname
-    newindex = int(mainname) * 2
-    indexL = "%05d" % (newindex - 1)
-    indexR = "%05d" % newindex
+    indexL = "%05d" % (2 * new_index)
+    indexR = "%05d" % (2 * new_index - 1)
     # print indexL, indexR
 
     img = Image.open(filename)
@@ -17,20 +17,55 @@ def split(filename):
     xaxishalf = xaxis/2
 
     leftimg = img.crop((0, 0, xaxishalf, yaxis))
-    leftimg.save('output/'+indexL+'.jpg', 'JPEG')
+    leftimg.save(prefix+indexL+'.jpg', 'JPEG')
     # leftimg.show()
 
     rightimg = img.crop((xaxishalf, 0, xaxis, yaxis))
     # rightimg.show()
-    rightimg.save('output/'+indexR+'.jpg', 'JPEG')
+    rightimg.save(prefix+indexR+'.jpg', 'JPEG')
+
+def comicsplit():
+    global new_index
+    new_index = 1
+    allcomic = []
+
+    all = os.listdir('.')
+    for comic in sorted(all):
+        main, sub = os.path.splitext(comic)
+        if 'jpg' in sub or 'JPEG' in sub or 'JPG' in sub:
+            allcomic.append(comic)
+
+    for jpg in allcomic:
+        if prefix in jpg:
+            # os.remove(jpg)
+            continue
+        print jpg
+
+        split(jpg)
+        new_index += 1
+
+def unzip():
+    for zname in glob('*.zip'):
+        print zname
+        zfile = zipfile.ZipFile(zname)
+        for name in zfile.namelist():
+            (dirname, filename) = os.path.splitext(zname)
+            if not os.path.exists(dirname):
+                os.makedirs(dirname)
+                print "Decompressing " + dirname
+            zfile.extract(name, dirname)
 
 def traverse():
-    if not os.path.exists('output'):
-        os.mkdir('output')
+    print os.getcwd()
+    unzip()
 
-    for jpg in glob('*.jpg'):
-        split(jpg)
+    allfile = os.listdir('.')
+    for comic in allfile:
+        if os.path.isdir(comic):
+            os.chdir(comic)
+            traverse()
+            comicsplit()
+            os.chdir('..')
 
 if __name__ == '__main__':
     traverse()
-    print 'done'
